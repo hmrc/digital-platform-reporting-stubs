e3d2732f8aa4acac494c41f5d7350dbac082ab20
Hristo Georgiev <georgievh@yahoo.co.uk>
2024-08-25 10:47:05 +0100

    Adds stubs for update and view of subscriptions.

--- app/uk/gov/hmrc/dprs/stubs/controllers/SubscribeController.scala
+++ app/uk/gov/hmrc/dprs/stubs/controllers/SubscribeController.scala
@@ -28,14 +28,37 @@ import javax.inject.{Inject, Singleton}
 @Singleton()
 class SubscribeController @Inject() (authFilter: AuthActionFilter, cc: ControllerComponents) extends BackendController(cc) with Logging {
 
-  private val subscriptionResponsePath = "/resources/subscription"
-  private val create_200_ResponsePath  = s"$subscriptionResponsePath/create/200-response.json"
+  private val subscriptionResponsePath          = "/resources/subscription"
+  private val create_200_ResponsePath           = s"$subscriptionResponsePath/create/200-response.json"
+  private val update_200_ResponsePath           = s"$subscriptionResponsePath/update/200-response.json"
+  private val view_200_OrganisationResponsePath = s"$subscriptionResponsePath/view/200-organisation-response.json"
+  private val view_200_IndividualResponsePath   = s"$subscriptionResponsePath/view/200-individual-response.json"
+  private val view_404_ResponsePath             = s"$subscriptionResponsePath/view/404-response.json"
 
   def create(): Action[JsValue] = (Action(parse.json) andThen authFilter) { implicit request =>
-    logger.info(s"User Subscription Request received: \n ${request.body} \n")
+    logger.info(s"Create User Subscription Request received: \n ${request.body} \n")
 
     (request.body \ "registerWithIDRequest" \ "requestDetail" \ "IDNumber").validate[String] match {
       case _ => Created(resourceAsString(create_200_ResponsePath))
+    }
+  }
+
+  def update(): Action[JsValue] = (Action(parse.json) andThen authFilter) { implicit request =>
+    logger.info(s"Update User Subscription Request received: \n ${request.body} \n")
+
+    (request.body \ "registerWithIDRequest" \ "requestDetail" \ "IDNumber").validate[String] match {
+      case _ => Ok(resourceAsString(update_200_ResponsePath))
+    }
+  }
+
+  def view(idValue: String): Action[JsValue] = (Action(parse.json) andThen authFilter) { implicit request =>
+    logger.info(s"View User Subscription Request received: \n ${request.body} \n")
+
+    idValue match {
+      case _ if idValue.contains("404") => InternalServerError(resourceAsString(view_404_ResponsePath))
+      case _ if idValue.startsWith("I") => Ok(resourceAsString(view_200_IndividualResponsePath))
+      case _ if idValue.startsWith("O") => Ok(resourceAsString(view_200_OrganisationResponsePath))
+      case _                            => BadRequest("Unexpected idValue.")
     }
   }
 }
