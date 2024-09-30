64f128603932ce92c631c728958f838991ea71c2
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-09-30 17:16:26 +0100

    Add SDES callback support

--- app/uk/gov/hmrc/dprs/stubs/services/SubmissionResultService.scala
+++ app/uk/gov/hmrc/dprs/stubs/services/SubmissionResultService.scala
@@ -16,28 +16,57 @@
 
 package uk.gov.hmrc.dprs.stubs.services
 
-import generated.BREResponse_Type
+import generated.{AEOI, BREResponse_Type, ErrorDetail_Type, FileError_Type, GenericStatusMessage_Type, RecordError_Type, RequestCommon_Type, RequestDetail_Type, ValidationErrors_Type, ValidationResult_Type}
 import org.apache.pekko.Done
-import play.api.Configuration
+import org.apache.pekko.actor.{ActorSystem, Cancellable, Scheduler}
+import play.api.{Configuration, Logging}
 import play.api.http.Status.NO_CONTENT
 import uk.gov.hmrc.dprs.stubs.config.Service
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps, UpstreamErrorResponse}
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 @Singleton
 class SubmissionResultService @Inject() (
                                           configuration: Configuration,
-                                          httpClient: HttpClientV2
-                                        )(implicit ec: ExecutionContext) {
+                                          httpClient: HttpClientV2,
+                                          actorSystem: ActorSystem
+                                        )(implicit ec: ExecutionContext) extends Logging {
 
   private val digitalPlatformReporting: Service = configuration.get[Service]("microservice.services.digital-platform-reporting")
   private val callbackAuthToken: String = configuration.get[String]("result-callback-auth-token")
+  private val resultDelay: FiniteDuration = configuration.get[FiniteDuration]("submission-result-delay")
 
-  def returnResult(submissionId: String, result: BREResponse_Type): Future[Done] = {
+  private val scheduler: Scheduler = actorSystem.scheduler
+
+  def scheduleSuccess(submissionId: String): Cancellable =
+    scheduleResponse(submissionId, successfulResponse(submissionId))
+
+  def scheduleFailure(submissionId: String): Cancellable =
+    scheduleResponse(submissionId, failedResponse(submissionId))
+
+  private def scheduleResponse(submissionId: String, response: BREResponse_Type): Cancellable = {
+    scheduler.scheduleOnce(resultDelay) {
+
+      logger.info(s"Returning callback from submission $submissionId")
+
+      returnResult(submissionId, response).onComplete {
+        case Success(_) =>
+          logger.info("Successfully sent callback")
+        case Failure(e) =>
+          logger.error("Problem sending callback", e)
+      }
+    }
+  }
+
+  private def returnResult(submissionId: String, result: BREResponse_Type): Future[Done] = {
     httpClient.post(url"$digitalPlatformReporting/cadx/submission/callback")(HeaderCarrier())
       .setHeader(
         "X-Correlation-ID" -> UUID.randomUUID().toString,
@@ -55,4 +84,53 @@ class SubmissionResultService @Inject() (
         }
       }
   }
+
+  private def successfulResponse(submissionId: String): BREResponse_Type =
+    BREResponse_Type(
+      requestCommon = RequestCommon_Type(
+        receiptDate = scalaxb.Helper.toCalendar(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now())),
+        regime = AEOI,
+        conversationID = submissionId,
+        schemaVersion = "1.0.0"
+      ),
+      requestDetail = RequestDetail_Type(
+        GenericStatusMessage = GenericStatusMessage_Type(
+          ValidationErrors = ValidationErrors_Type(
+            FileError = Seq.empty,
+            RecordError = Seq.empty
+          ),
+          ValidationResult = ValidationResult_Type(
+            Status = generated.Accepted
+          )
+        )
+      )
+    )
+
+  private def failedResponse(submissionId: String): BREResponse_Type =
+    BREResponse_Type(
+      requestCommon = RequestCommon_Type(
+        receiptDate = scalaxb.Helper.toCalendar(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now())),
+        regime = AEOI,
+        conversationID = submissionId,
+        schemaVersion = "1.0.0"
+      ),
+      requestDetail = RequestDetail_Type(
+        GenericStatusMessage = GenericStatusMessage_Type(
+          ValidationErrors = ValidationErrors_Type(
+            FileError = Seq(FileError_Type(
+              Code = "001",
+              Details = Some(ErrorDetail_Type("detail"))
+            )),
+            RecordError = Seq(RecordError_Type(
+              Code = "002",
+              Details = Some(ErrorDetail_Type("detail 2")),
+              DocRefIDInError = Seq("1", "2")
+            ))
+          ),
+          ValidationResult = ValidationResult_Type(
+            Status = generated.Rejected
+          )
+        )
+      )
+    )
 }
